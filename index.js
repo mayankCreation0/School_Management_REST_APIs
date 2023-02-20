631ec9a0d64c6d36c4d2d791312dfc18d997348b
Mayank Raj <mayankrj1224@gmail.com>
2023-02-20 21:17:37 +0530

    updated

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const app = express();
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
+app.use('/', routers),
+    app.get('/', (req, res) => {
+        res.send("Server is Live")
+    })
 app.use("/", adminRouter);
 app.use("/", teacherRouter);
 
 const PORT = 5000 || process.env.PORT;
 connect()
-.then(()=>(
-    app.listen(PORT,()=>{
-        console.log(`listening on port ${PORT}`);
-    })
-))
-.catch((error)=>(
-    console.log(error)
-))
+    .then(() => (
+        console.log("listening")
+    ))
+    .catch((error) => (
+        console.log(error)
+    ))
+app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+})
